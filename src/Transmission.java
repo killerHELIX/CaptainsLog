c38eb12b389f49c3c488ceaebf1f261b92ac831d
killerHELIX <rharris4@mail.umw.edu>
2016-03-31 10:56:10 -0400

    added fields and methods

--- src/Transmission.java
+++ src/Transmission.java
@@ -1,5 +1,34 @@
+import java.util.ArrayList;
+
 /**
  * Created by ryanharris on 3/28/16.
  */
 public class Transmission {
+
+    private String message;
+    private ArrayList<String> hashtag = new ArrayList<>();
+    private boolean visibility = true;
+    private User target = null;
+    private User author;
+    private ArrayList<User> favoritedBy = new ArrayList<>();
+
+    public User viewAuthor(){
+
+        return author;
+    }
+
+    public String getMessage(){
+
+        return message;
+    }
+
+    public ArrayList<String> getHashtag(){
+
+        return hashtag;
+    }
+
+    public boolean isPublic(){
+
+        return visibility;
+    }
 }
