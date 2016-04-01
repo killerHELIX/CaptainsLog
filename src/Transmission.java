817d4f5aa429eb8f41cdfc4671ab24aa4979d2d4
killerHELIX <rharris4@mail.umw.edu>
2016-04-01 12:10:53 -0400

    added constructors

--- src/Transmission.java
+++ src/Transmission.java
@@ -9,6 +9,33 @@ public class Transmission {
     private User author;
     private ArrayList<User> favoritedBy = new ArrayList<>();
 
+    /**
+     *
+     * @param message the content of the transmission
+     * @param hashtag the hashtag for the transmission
+     * @param target the targets meant to receive the transmission
+     * @param author the User who created the transmission
+     */
+    Transmission (String message, String hashtag, User target, User author){
+        this.message = message;
+        this.hashtag.add(hashtag);
+        this.target = target;
+        this.author = author;
+    }
+
+    /**
+     *
+     * @param message the content of the transmission
+     * @param target the hashtag for the transmission
+     * @param author the User who created the transmission
+     */
+    Transmission (String message, User target, User author){
+
+        this.message = message;
+        this.target = target;
+        this.author = author;
+    }
+
     public User viewAuthor(){
 
         return author;
