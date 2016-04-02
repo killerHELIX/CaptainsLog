4960bb0e8fa36ecb7c894d706026dc43f69d99cf
James Murphy <jmurphy4@mail.umw.edu>
2016-04-02 12:39:54 -0400

    Created constructor

--- src/User.java
+++ src/User.java
@@ -4,45 +4,55 @@ public class User implements Menu {
 
     private String username;
     private String displayName;
-    private ArrayList<String[]> photo = new ArrayList<>();
-    private ArrayList<Transmission> history = new ArrayList<>();
-    private ArrayList<User> blacklist = new ArrayList<>();
-    private ArrayList<User> followers = new ArrayList<>();
-    private ArrayList<User> following = new ArrayList<>();
+    private String[] photo;
+    private ArrayList<Transmission> history;
+    private ArrayList<User> blacklist;
+    private ArrayList<User> followers;
+    private ArrayList<User> following;
     private int password;
     
-    public String getUsername()
-    {
+	public User(String usrnm, String dspnm, passwd) {
+		history = new ArrayList<>();
+		blacklist = new ArrayList<>();
+		followers = new ArrayList<>();
+		following = new ArrayList<>();
+		
+		photo = null;
+		username = usrnm;
+		displayName = dspnm;
+	}
+	
+    public String getUsername() {
         return username;
     }
     
-    public String getDisplayName()
-    {
+    public String getDisplayName() {
         return displayName;
     }
     
-    public void setDisplayName(String dN)
-    {
+    public void setDisplayName(String dN) {
         dN = displayName;
     }
     
-    public ArrayList<String[]> getPhoto()
-    {
+    public ArrayList<String[]> getPhoto() {
         return photo;
     }
     
-    public void setPhoto(ArrayList<String[]> ph)
-    {
-        ph = photo;
-    }
+    public void setPhoto(String filename) {
+		try {
+        	photo = getASCIIart(filename, true);
+    	} catch (IOException e) {
+			photo = null;
+		}
+	}
     
-    public void sortByTime(User currentUser){
+    public void sortByTime(User currentUser) {
+
+    } 
+    public void sortByPopularity(User currentUser) {
 
     }
-    public void sortByPopularity(User currentUser){
-
-    }
-    public User searchForUser(String user){
+    public User searchForUser(String user) {
 
     }
     public Transmission searchForTransmission(String transmission){
@@ -60,5 +70,4 @@ public class User implements Menu {
     public void modifySettings(User currentUser){
 
     }
-
 }
