f48b90c8ed28d1e72a2da64bace76fb28c652026
killerHELIX <rharris4@mail.umw.edu>
2016-04-02 12:12:13 -0400

    added Menu method overrides

--- src/User.java
+++ src/User.java
@@ -11,4 +11,29 @@ public class User implements Menu {
     private ArrayList<User> following = new ArrayList<>();
     private int password;
 
+    public void sortByTime(User currentUser){
+
+    }
+    public void sortByPopularity(User currentUser){
+
+    }
+    public User searchForUser(String user){
+
+    }
+    public Transmission searchForTransmission(String transmission){
+
+    }
+    public void searchByHashtag(String hashtag){
+
+    }
+    public boolean login(String password, User selectedUser){
+
+    }
+    public boolean logout(){
+
+    }
+    public void modifySettings(User currentUser){
+
+    }
+
 }
