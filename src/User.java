98923200b76ed7a4b9139ad8b54c4a8d20f3d46e
killerHELIX <rharris4@mail.umw.edu>
2016-04-02 13:06:53 -0400

    fixed password again

--- src/User.java
+++ src/User.java
@@ -11,86 +11,41 @@ public class User extends Menu {
     private ArrayList<User> followers;
     private ArrayList<User> following;
     private String password;
-    
-	public User (String usrnm, String dspnm, String passwd) {
-		history = new ArrayList<>();
-		blacklist = new ArrayList<>();
-		followers = new ArrayList<>();
-		following = new ArrayList<>();
-		
-		photo = null;
-		username = usrnm;
-		displayName = dspnm;
-<<<<<<< HEAD
-                password = passwd;
-=======
-		password = passwd;
->>>>>>> 42d0b29b35ba211607ba7fb45708bb808cda80fb
-	}
-	
-	public String getUsername() {
-		return username;
-	}
-	
-	public String getDisplayName() {
-		return displayName;
-	}
-	
-	public void setDisplayName(String dN) {
-		displayName = dN;
-	}
-	
-	public String[] getPhoto() {
-		return photo;
-	}
-	
-	public void setPhoto(String filename) {
-		try {
-			photo = getASCIIArt(filename, true);
-		} catch (IOException e) {
-			photo = null;
-		}
-	}
-<<<<<<< HEAD
-    
-    public ArrayList<Transmission> getHistory()
-    {
-        return history;
+
+    public User(String usrnm, String dspnm, String passwd) {
+        history = new ArrayList<>();
+        blacklist = new ArrayList<>();
+        followers = new ArrayList<>();
+        following = new ArrayList<>();
+
+        photo = null;
+        username = usrnm;
+        displayName = dspnm;
+        password = passwd;
+
     }
-    
-    public void addToHistory(Transmission tr)
-    {
-        
+
+    public String getUsername() {
+        return username;
     }
-=======
-	
-	public void sortByTime(User currentUser) {
 
-	} 
-	public void sortByPopularity(User currentUser) {
+    public String getDisplayName() {
+        return displayName;
+    }
 
-	}
-	public User searchForUser(String user) {
+    public void setDisplayName(String dN) {
+        displayName = dN;
+    }
 
-	}
-	public Transmission searchForTransmission(String transmission){
+    public String[] getPhoto() {
+        return photo;
+    }
 
-	}
-	public void searchByHashtag(String hashtag){
-
-	}
-	public boolean login(String password, User selectedUser){
-
-	}
-	public boolean logout(){
-
-	}
-	public void modifySettings(User currentUser){
-
-	}
-	
-	public ArrayList<Transmission> getHistory() {
-		return history;
-	}
->>>>>>> 42d0b29b35ba211607ba7fb45708bb808cda80fb
+    public void setPhoto(String filename) {
+        try {
+            photo = getASCIIArt(filename, true);
+        } catch (IOException e) {
+            photo = null;
+        }
+    }
 }
