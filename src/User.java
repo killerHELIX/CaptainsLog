691685ba692acbcd5f0a110698ba20350598dc54
jm0nsta96 <jwilli28@mail.umw.edu>
2016-04-02 12:49:28 -0400

    .

--- src/User.java
+++ src/User.java
@@ -11,7 +11,7 @@ public class User extends Menu {
     private ArrayList<User> following;
     private int password;
     
-	public User (String usrnm, String dspnm, passwd) {
+	public User (String usrnm, String dspnm, int passwd) {
 		history = new ArrayList<>();
 		blacklist = new ArrayList<>();
 		followers = new ArrayList<>();
@@ -20,6 +20,7 @@ public class User extends Menu {
 		photo = null;
 		username = usrnm;
 		displayName = dspnm;
+                password = passwd;
 	}
 	
     public String getUsername() {
