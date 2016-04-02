9efe37ccffb634e273f0081dfe073a58bcfdc629
jm0nsta96 <joshwilliams@10.214.13.48>
2016-04-02 11:37:57 -0400

    Added all of the data members of the User class.

--- src/User.java
+++ src/User.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
 
 public class User {
+    
+    private String username;
+    private String displayName;
+    private ArrayList<String[]> photo = new ArrayList<>();
+    private ArrayList<Transmission> history = new ArrayList<>();
+    private ArrayList<User> blacklist = new ArrayList<>();
+    private ArrayList<User> followers = new ArrayList<>();
+    private ArrayList<User> following = new ArrayList<>();
+    private int password;
+    
+    
 }
