e3f6347e74d30a026f988e0f5884235459157f25
killerHELIX <rharris4@mail.umw.edu>
2016-04-04 15:12:26 -0400

    added master arraylists

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class Main {
 	public static void main(String[] args) {
+
+        ArrayList<User> masterUserList = new ArrayList<>();
+        ArrayList<Transmission> masterTransmissionList = new ArrayList<>();
+        
 		System.out.println(" --- ");
 	}
 }
