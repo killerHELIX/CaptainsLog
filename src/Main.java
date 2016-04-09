c22cc9dacc3baa654865e31c070f6928c4b9b043
killerHELIX <rharris4@mail.umw.edu>
2016-04-09 11:43:57 -0400

    added first iteration of main, added template cases to switch statement

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,70 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 
+        Scanner in = new Scanner(System.in);
+        Menu menu = new Menu();
+        User currentUser;
+
         ArrayList<User> masterUserList = new ArrayList<>();
         ArrayList<Transmission> masterTransmissionList = new ArrayList<>();
-                
-        System.out.println(" --- ");
+
+        boolean isInMenu = true;
+        boolean isLoggedIn = false;
+
+        if (!isLoggedIn){
+
+            currentUser = menu.login(in, masterUserList);
+
+            if (currentUser != null){
+                isLoggedIn = true;
+            } else {
+                System.out.println("Access denied.  Exiting...");
+            }
+        }
+
+        while (isInMenu && isLoggedIn){
+
+            System.out.println("Enter your choice: ");
+            switch(in.next()){
+
+                case "sortByTime":
+                    System.out.println("you chose 'a' ");
+                    break;
+
+                case "sortByPopularity":
+                    System.out.println("you chose 'b' ");
+                    break;
+
+                case "searchForUser":
+                    // TODO
+
+                    break;
+
+                case "searchForTransmission":
+                    // TODO
+
+                    break;
+
+                case "searchByHashtag":
+                    // TODO
+
+                    break;
+
+                case "logout":
+                    // TODO
+
+                    break;
+
+                case "modifySettings":
+                    // TODO
+
+                    break;
+
+            }
+
+        }
 	}
 }
